4ea58d9ca7a9cd0dd5a774c7b9b355e7d355cb0d
Ewan Barr <ewan.d.barr@googlemail.com>
2018-07-12 10:23:42 +0200

    Added setup.py and restructured

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='mosaic',
+      version='0.1',
+      description='Code for generating point spread functions and beam tiling for radio interferometers',
+      url='https://gitlab.mpifr-bonn.mpg.de/wchen/Beamforming/',
+      author='Weiwei Chen',
+      author_email='wchen@mpifr-bonn.mpg.de',
+      license='MIT',
+      packages=['mosaic'],
+      install_requires=[
+          'scipy',
+          'numpy',
+          'matplotlib',
+          'katpoint',
+          'h5py',
+          'astropy'
+      ],
+      zip_safe=False)
