[fix]: azimuth calculation error when it's around -180 [fix]: angle range in interferometry plot [add]: example of overview plot

mosaic/coordinate.py
@@ -124,7 +124,10 @@ def convertEquatorialToHorizontal(RA, DEC, LST, latitude):
     '''local hour angle'''
     LHA = LST  - RA
     altitude = np.arcsin(np.sin(latitude)*np.sin(DEC) + np.cos(latitude)*np.cos(DEC)*np.cos(LHA))
-    azimuth = np.arccos((np.sin(DEC) - np.sin(altitude)*np.sin(latitude))/(np.cos(altitude)*np.cos(latitude)))
+    val = (np.sin(DEC) - np.sin(altitude)*np.sin(latitude))/(np.cos(altitude)*np.cos(latitude))
+    if (np.abs(val) > 1 and (np.abs(val) - 1) < 0.000000001):
+       val = np.trunc(val)
+    azimuth = np.arccos(val)
 
     mask = np.sin(LHA) > 0.0
     azimuth[mask] = np.pi*2 - azimuth[mask]

mosaic/plot.py
@@ -325,16 +325,16 @@ def plot_all(interferometry, beamshape, overlap, retile, fileName):
     """
     def angleToCartesian(angleDeg, radius):
         angle = np.deg2rad(angleDeg)
-        if angle < np.pi/2:
+        if angle <= np.pi/2:
             x = np.sin(angle)*radius
             y = np.cos(angle)*radius
-        elif angle > np.pi/2 and angle < np.pi:
+        elif angle > np.pi/2 and angle <= np.pi:
             x = np.sin(angle)*radius
             y = np.cos(angle)*radius
-        elif angle > np.pi and angle < np.pi*1.5:
+        elif angle > np.pi and angle <= np.pi*1.5:
             x = np.sin(angle)*radius
             y = np.cos(angle)*radius
-        elif angle > np.pi*1.5 and angle < np.pi*2:
+        elif angle > np.pi*1.5 and angle <= np.pi*2:
             x = np.sin(angle)*radius
             y = np.cos(angle)*radius
 